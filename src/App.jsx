996b147eb2654f6d26890dd19c8b0ecd00623c50
Varun <varunvaru524@gmail.com>
2024-02-04 10:55:30 +0530

    Updating home screen banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useEffect } from 'react'
-import './App.css'
+import styles from './App.module.css';
+import image from './assets/Antarac-website.png'
 
 function App() {
-  useEffect(() => {
-    window.location.href = 'https://www.figma.com/proto/cH7dtaWg83LROUHctHJSNz/Antarac%3A-The-Activity-App-(A-brief-intro)?node-id=1-2&type=design&t=5fHFcOLq2vxLpnV8-1&scaling=scale-down&page-id=0%3A1&locale=en'
-  }, [])
-  return <h1>Antarac</h1>
+  return (
+    <img src={image} className={styles.img} alt="Antarac" />
+  )
 }
 
 export default App
