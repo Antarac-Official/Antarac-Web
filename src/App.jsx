ec35ea1085f23c2db81c5acf35552b693b5e8407
Varun <varunvaru524@gmail.com>
2024-02-27 00:40:36 +0530

    Now the website and its pages points to a framer website tempararily

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { useEffect } from 'react';
 
-
 function App() {
+
   useEffect(() => {
-    window.location.href = 'https://antarac.framer.website/'
-  }, [])
+    const pathname = window.location.pathname;
+
+    if (pathname === '/') window.location.href = 'https://antarac.framer.website/';
+    else if (pathname === '/contact-us') window.location.href = 'https://antarac.framer.website/contact';
+    else if (pathname === '/terms-of-service') window.location.href = 'https://antarac.framer.website/terms';
+    else if (pathname === '/privacy-policy') window.location.href = 'https://antarac.framer.website/privacy';
+    else window.location.href = 'https://antarac.framer.website/'
+  }, []);
+
   return (
     <></>
-  )
+  );
 }
-
 
 export default App
