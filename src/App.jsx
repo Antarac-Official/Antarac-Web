ba1ec220dd1461efaf151070f820a31c9b5716ee
Varun <varunvaru524@gmail.com>
2024-10-06 22:38:50 +0530

    Started to build v2 of launching soon

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
-import { useEffect } from 'react';
+import HomePage from './Pages/HomePage';
 
 function App() {
-
-  useEffect(() => {
-    const pathname = window.location.pathname;
-
-    if (pathname === '/') window.location.href = 'https://antarac.framer.website/';
-    else if (pathname === '/contact-us') window.location.href = 'https://antarac.framer.website/contact';
-    else if (pathname === '/terms-of-service') window.location.href = 'https://antarac.framer.website/terms';
-    else if (pathname === '/privacy-policy') window.location.href = 'https://antarac.framer.website/privacy';
-    else window.location.href = 'https://antarac.framer.website/'
-  }, []);
-
   return (
-    <></>
+    <>
+      <HomePage />
+    </>
   );
 }
 
