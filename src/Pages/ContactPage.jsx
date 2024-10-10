2f5f546302b95833a16510c9ae66f169634d7e0c
Varun <varunvaru524@gmail.com>
2024-10-11 00:51:00 +0530

    Started building contact us page

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './ContactPage.module.css'
+import antaracFullLogo from '../assets/fullLogo.svg';
+import linkedinLogo from '../assets/linkedinLogo.svg';
 
 const ContactPage = () => {
   return (
-    <div>
-      <h1>Contact Page</h1>
-      <p>Welcome to the Contact Page!</p>
+    <div className={styles.container}>
+      <main className={styles.mainContainer}>
+        <img src={antaracFullLogo} alt="Antarac logo" className={styles.logo} />
+        <div className={styles.contactusContainer}>
+          <div className={styles.contactusStrik}></div>
+          <div className={styles.contactusText}>Contact Us</div>
+        </div>
+      </main>
+      <footer className={styles.footer}>
+        <div className={styles.footerContainer}>
+          <div className={styles.footerLeftInfo}>© 2024 Antarac Social Pvt Ltd</div>
+          <div className={styles.footerCenterInfo}>
+            <a href="" className={styles.footerNavText}>Careers</a>
+            <Link to="/contact" className={styles.footerNavText} style={{ color: 'black' }}>Contact</Link>
+          </div>
+          <div className={styles.footerRightInfo}>
+            <img src={linkedinLogo} alt="Linkedin Logo" className={styles.linkedinLogo} />
+          </div>
+        </div>
+      </footer>
     </div>
   );
 };
