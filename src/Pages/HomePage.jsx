cda72c10e0a9f6effc1a1f46406ba7747cbf6e2b
Varun <varunvaru524@gmail.com>
2024-10-10 23:59:17 +0530

    Built home screen

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from './HomePage.module.css'
+import mockupDesktop from '../assets/mockupDesktop.png';
+import antaracFullLogo from '../assets/fullLogo.svg';
+import linkedinLogo from '../assets/linkedinLogo.svg';
 
 function HomePage() {
+  const [value, setValue] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('submited', value)
+  }
+
   return (
-    <>
-      <div>Hey</div>
-    </>
+    <div className={styles.container}>
+      <main className={styles.mainContainer}>
+        <div className={styles.insideMain}>
+          <div className={styles.mainLeftContainer}>
+            <img src={mockupDesktop} alt="App mockup image" className={styles.mockupDesktop} />
+          </div>
+          <div className={styles.mainRightContainer}>
+            <div className={styles.insideRightContainer}>
+              <img src={antaracFullLogo} alt="Antarac logo" className={styles.rightLogoContainer} />
+              <div className={styles.rightInfo}>An efficient platform catering to a primal human desire - Social Connection.</div>
+              <div className={styles.availableSoonContainer}>
+                <div className={styles.availableSoonStrik}></div>
+                <div className={styles.availableSoonText}>AVAILABLE SOON</div>
+              </div>
+              <form onSubmit={handleSubmit}>
+                <div className={styles.formDescriptionContainer}>
+                  <div className={styles.formDescription}>Be the first to experience. No spams. Promise!</div>
+                </div>
+                <input value={value} onChange={(e) => setValue(e.target.value)} type="text" placeholder="Enter your email" className={styles.formInput} />
+                <button onSubmit={handleSubmit} className={styles.submitButton}>Notify me</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer className={styles.footer}>
+        <div className={styles.footerContainer}>
+          <div className={styles.footerLeftInfo}>© 2024 Antarac Social Pvt Ltd</div>
+          <div className={styles.footerCenterInfo}>
+            <a href="" className={styles.footerNavText}>Careers</a>
+          </div>
+          <div className={styles.footerRightInfo}>
+            <img src={linkedinLogo} alt="Linkedin Logo" className={styles.linkedinLogo} />
+          </div>
+        </div>
+      </footer>
+    </div>
   )
 }
 
